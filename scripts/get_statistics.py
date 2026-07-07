6ea35905cd50d43352399742f865da11aa6f256e
miznchimaki <miznchimaki@gmail.com>
2026-07-07 14:38:25 +0800

    have finished studying the source code in scripts/get_statistics.py

--- scripts/get_statistics.py
+++ scripts/get_statistics.py
@@ -16,7 +16,7 @@ def cal_statistic(data, _filter=True):
     q99 = np.percentile(data, 99, axis=0)
     q01 = np.percentile(data,  1, axis=0)
     if _filter:
-        data_mask = (data>=q01) & (data <= q99)
+        data_mask = (data >= q01) & (data <= q99)
         data_mask = data_mask.min(axis=1)
         data = data[data_mask, :]
     means = np.mean(data, axis=0)
@@ -34,7 +34,7 @@ def get_statistics(
     nrnd=50000,
     _filter=True
 ):
-    assert(data_type in ["joint", "eef"])
+    assert (data_type in ["joint", "eef"])
 
     data_path_list = os.listdir(data_root)
     data_path_list.sort()
@@ -47,37 +47,37 @@ def get_statistics(
     for data_path in tqdm.tqdm(data_path_list):
         data = load_data(os.path.join(data_root, data_path), action_key)
         data_list.append(data)
-        delta_data = data[1:] - data[:-1]
+        delta_data = data[1: ] - data[: -1]
         delta_data_list.append(delta_data)
         state = load_data(os.path.join(data_root, data_path), state_key)
         state_list.append(state)
 
     data_list = np.concatenate(data_list, axis=0)
-    assert(len(data_list.shape)==2)
+    assert (len(data_list.shape) == 2)
     means, stds, q99, q01 = cal_statistic(data_list, _filter=_filter)
 
     delta_data_list = np.concatenate(delta_data_list, axis=0)
-    assert(len(delta_data_list.shape)==2)
+    assert (len(delta_data_list.shape) == 2)
     delta_means, delta_stds, delta_q99, delta_q01 = cal_statistic(delta_data_list, _filter=_filter)
 
     state_list = np.concatenate(state_list, axis=0)
-    assert(len(state_list.shape)==2)
+    assert (len(state_list.shape) == 2)
     state_means, state_stds, state_q99, state_q01 = cal_statistic(state_list, _filter=_filter)
 
     statistics_info = dict({
-        data_name+"_"+data_type:dict({
+        data_name + "_" + data_type: dict({
             "mean": means.tolist(),
             "std": stds.tolist(),
             "q99": q99.tolist(),
             "q01": q01.tolist(),
         }),
-        data_name+"_delta_"+data_type:dict({
+        data_name + "_delta_" + data_type: dict({
             "mean": delta_means.tolist(),
             "std": delta_stds.tolist(),
             "q99": delta_q99.tolist(),
             "q01": delta_q01.tolist(),
         }),
-        data_name+"_state_"+data_type:dict({
+        data_name + "_state_" + data_type: dict({
             "mean": state_means.tolist(),
             "std": state_stds.tolist(),
             "q99": state_q99.tolist(),
