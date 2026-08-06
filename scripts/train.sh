8e8e653b8169c32549244fd03d5f30fb0edea619
miznchimaki <miznchimaki@gmail.com>
2026-08-06 17:32:02 +0800

    debugging ...

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 
 
+source ${HOME}/.bashrc
+
 script_path=${1}
 echo $script_path
 
@@ -9,16 +11,30 @@ echo $config_path
 
 if [ -z $WORLD_SIZE ]; then
 NGPU=`nvidia-smi --list-gpus | wc -l`
+NGPU=6
+
+# TODO: (mizuno) The following environmental variable is useful for torch2.6/2.7/2.8+cu126 + Python3.13
+# export TORCHINDUCTOR_COMPILE_THREADS=1
+
+cd ${HOME}/projects/Genie-Envisioner/
 echo "Training on 1 Nodes, $NGPU GPUs"
-torchrun --nnodes=1 \
+# TODO: (mizuno) The following command is for torch2.6/2.7/2.8+cu126 + Python3.13
+# lerobot310-python -m torch.distributed.run --nnodes=1 \
+#     --nproc_per_node=$NGPU \
+#     --node_rank=0 \
+#     --no_python \
+#     lerobot310-python \
+#     $script_path \
+#     --config_file $config_path
+lerobot310-python -m torch.distributed.run --nnodes=1 \
     --nproc_per_node=$NGPU \
     --node_rank=0 \
     $script_path \
-    --config_file $config_path
+    --config_file $config_path  2>&1 | tee logs/debug.log
 else
 echo "Training on $WORLD_SIZE Nodes, 8 GPU per Node"
 NGPU=`nvidia-smi --list-gpus | wc -l`
-torchrun --nnodes=$WORLD_SIZE \
+lerobot310-python -m torch.distributed.run --nnodes=$WORLD_SIZE \
     --nproc_per_node=$NGPU \
     --node_rank=$RANK \
     --master-addr $MASTER_ADDR \
